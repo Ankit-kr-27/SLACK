f4e5f4a71c27041dc798f87546f274bb6c490280
Ankit kumar <kr.ankit2783@gmail.com>
2025-08-31 11:09:29 +0530

    db connected and env.js completed

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import { ENV } from './config/env.js';
+import { connectDB } from './config/db.js';
 
 
 const app = express();
@@ -10,4 +11,7 @@ app.get("/",(req,res) => {
     res.send("hello world");
 });
 
-app.listen(ENV.PORT, () => console.log("server is running on port:",ENV.PORT));
+app.listen(ENV.PORT, () => {
+    console.log("server is running on port:",ENV.PORT)
+    connectDB()
+});
